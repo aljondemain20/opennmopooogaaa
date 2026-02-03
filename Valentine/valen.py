aaec94a9d729d1409e72563d2752409094dafae1
aljondemain20 <aljondemain20@gmail.com>
2026-02-03 02:02:33 -0800

    Add files via upload

---
+++ Valentine/valen.py
@@ -0,0 +1,25 @@
+#wasaaaaa
+from flask import Flask, render_template, url_for, redirect
+
+app = Flask(__name__)
+no_clicks = 0
+
+@app.route('/for-my-valentine-rachell')
+def index():
+    return render_template('index.html', no_clicks=no_clicks)
+
+@app.route('/yes')
+def yes():
+    global no_clicks
+    no_clicks = 0
+    return render_template('yes.html')
+
+@app.route("/no")
+def no():
+    global no_clicks
+    no_clicks += 1
+    return redirect(url_for("index"))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
