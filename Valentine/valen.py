d561b8c72920df0072d4fa254b183f50deb0c4ee
aljondemain20 <aljondemain20@gmail.com>
2026-02-03 04:35:23 -0800

    Update valen.py

--- Valentine/valen.py
+++ Valentine/valen.py
@@ -1,31 +1,33 @@
-#wasaaaaa
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, abort
 
 app = Flask(__name__)
+
 no_clicks = 0
+
 
 @app.route("/")
 def blocked():
     abort(404)
-    
-@app.route("/for-my-langgaaa onlyyy-rachell-0214")
-def secret():
-    return render_template('index.html', no_clicks=no_clicks)
 
-@app.route('/yes')
+
+@app.route("/for-my-langgaaa-onlyyy-rachell-0214")
+def secret():
+    return render_template("index.html", no_clicks=no_clicks)
+
+
+@app.route("/yes")
 def yes():
     global no_clicks
     no_clicks = 0
-    return render_template('yes.html')
+    return render_template("yes.html")
+
 
 @app.route("/no")
 def no():
     global no_clicks
     no_clicks += 1
-    return redirect(url_for("index"))
+    return redirect(url_for("secret"))
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=50000)
-
-
+    app.run(host="0.0.0.0", port=5000)
